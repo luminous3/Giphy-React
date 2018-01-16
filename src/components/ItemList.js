cceff87f7e9686ec55a1b393df03de62dc24ac6d
alex <alex@gmail.com>
2018-01-16 13:46:16 -0800

    add loading indicator

--- src/components/ItemList.js
+++ src/components/ItemList.js
@@ -6,10 +6,6 @@ const LIMIT = 10;
 const API_KEY = 'o0WkAgsV8Yu0S7pjGI1Bk594LxB49hGF';
 const RATING = 'G';
 const GIPHY_BASE_URL = 'https://api.giphy.com/v1/gifs/';
-// const API_KEY_STRING = `?api_key=${API_KEY}`;
-// const TRENDING = 'trending?';
-// const LIMIT_STR = `&limit=${LIMIT}`;
-// const RATING_STR = `&rating=${G}`;
 
 export class ItemList extends Component {
   constructor(props) {
@@ -17,25 +13,9 @@ export class ItemList extends Component {
 
     this.state = {
       items: [],
-      searchValue: ''
+      searchValue: '',
+      loading: false
     };
-  }
-
-  renderItems() {
-    const { items } = this.state;
-    return items.map(({ id, images, url }) => {
-      return (
-        <li key={id} className="container-item">
-          <a href={url} target="_blank">
-            <img
-              src={images.fixed_width_downsampled.url}
-              className="preview"
-              alt="Gif Preview"
-            />
-          </a>
-        </li>
-      );
-    });
   }
 
   componentDidMount() {
@@ -49,16 +29,78 @@ export class ItemList extends Component {
       });
   }
 
+  handleKeyPress(event) {
+    if (event.charCode === 13) {
+      document.getElementById('search').blur();
+      this.handleSearch();
+    }
+  }
+
+  handleSearch() {
+    const { searchInput } = this.state;
+    this.setState({
+      loading: true
+    });
+    axios
+      .get(
+        `https://api.giphy.com/v1/gifs/search?api_key=o0WkAgsV8Yu0S7pjGI1Bk594LxB49hGF&q=${searchInput}&limit=100&offset=0&rating=G&lang=en`
+      )
+      .then(res => {
+        console.log(res.data.data);
+        this.setState({
+          loading: false,
+          items: res.data.data
+        });
+      });
+  }
+
+  renderItems() {
+    const { items } = this.state;
+    return items.map(({ id, images, url }) => {
+      return (
+        <li key={id} className="gif-item">
+          <a href={url} target="_blank">
+            <img
+              src={images.fixed_width_downsampled.url}
+              className="preview"
+              alt="Gif Preview"
+            />
+          </a>
+        </li>
+      );
+    });
+  }
+
   render() {
+    const { loading } = this.state;
     return (
       <div className="wrapper">
         <input
+          id="search"
+          onKeyPress={this.handleKeyPress.bind(this)}
           onChange={event => this.setState({ searchValue: event.target.value })}
           value={this.state.searchValue}
-          placeholder={'Search GIFs'}
+          placeholder={'Search All GIFs'}
         />
         <div className="trending-title">Top Trending Gifs</div>
-        <ul className="container">{this.renderItems()}</ul>
+        {!loading && (
+          <div className="loading-container">
+            <div className="preloader-wrapper big active">
+              <div className="spinner-layer spinner-red-only">
+                <div className="circle-clipper left">
+                  <div className="circle" />
+                </div>
+                <div className="gap-patch">
+                  <div className="circle" />
+                </div>
+                <div className="circle-clipper right">
+                  <div className="circle" />
+                </div>
+              </div>
+            </div>
+          </div>
+        )}
+        {loading && <ul className="gif-container">{this.renderItems()}</ul>}
       </div>
     );
   }
